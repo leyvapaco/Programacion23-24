3dff769c30100c8a573984c1517afb7333fd424e
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2024-02-01 11:09:20 +0100

    Ejercicios Hora

---
+++ src/u5/t1/actividad4/Principal.java
@@ -0,0 +1,33 @@
+package u5.t1.actividad4;
+
+
+
+public class Principal {
+	public static void main(String[] args) {
+		Hora12 hora12 = new Hora12(11, 55, "am");
+		
+		System.out.println(hora12.toString());
+		
+		for(int i=0; i<10; i++) hora12.inc();
+		
+		System.out.println(hora12.toString());
+		
+		for(int j=0; j<55; j++) hora12.inc();
+		
+		System.out.println(hora12.toString());
+		
+		for(int j=0; j<200; j++) hora12.inc();
+		
+		System.out.println(hora12.toString());
+		
+		for(int j=0; j<50; j++) hora12.inc();
+		
+		System.out.println(hora12.toString());
+		
+		for(int j=0; j<50; j++) hora12.inc();
+		
+		System.out.println(hora12.toString());
+		
+
+	}
+}
